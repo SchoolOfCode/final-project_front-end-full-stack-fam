5096442be46d1ed844a7eb17a0d91ad12392b049
nas <nasraxsaleh@hotmail.com>
2022-07-27 11:33:50 +0100

    frontend set up

---
+++ frontend-final-project/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const url = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:3000";
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Hello </h1>
+    </div>
+  );
+}
+
+export default App;
