2d68e960ea8c03d4cd7d0ca658c91ccd0b0880d1
Wisteria Hawthorn <92452890+wisteria-hawthorn@users.noreply.github.com>
2022-08-01 11:48:10 +0100

    component logic in progress

--- frontend-final-project/src/routes/ActivityPage/ActivityPage.js
+++ frontend-final-project/src/routes/ActivityPage/ActivityPage.js
@@ -11,6 +11,28 @@ export default function ActivityPage() {
           </p>
         </main>
         
+
+        <div className='num-line-container'>
+            <section className='num-grid'>
+                <p>2</p>
+                <p>4</p>
+                <p>6</p>
+                <p>8</p>
+                <p>10</p>
+                <p>12</p>
+                <p>14</p>
+                <p>16</p>
+                <p>18</p>
+                <p>20</p>
+            </section>
+    
+            <section className='num-line'></section>
+        </div>
+
+
+
+
+
         <nav>
           <Link to="/login">Back to Log In</Link>
         </nav>
