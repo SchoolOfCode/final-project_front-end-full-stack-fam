3b269c08a094ed87e3547695a20f7eb32f3e6a5e
Ceejay222 <103362469+Ceejay222@users.noreply.github.com>
2022-08-02 00:02:49 +0100

    Updated useState values and function to spread useState and change values via Handlchange.

--- frontend-final-project/src/routes/SignupPage/SignupForm.js
+++ frontend-final-project/src/routes/SignupPage/SignupForm.js
@@ -10,9 +10,18 @@ export default function SignupForm() {
         password: "",
         parent4DigitPassword: "",
         childsName: ""
-    })
+    });
 
+    function handleFormSubmit (event) {
+        event.preventDefault();
+    }
 
+    function handleChange (event) {
+        setsignUpVales({
+            ...signUpValues,
+            [event.target.name]: event.target.value,
+        });
+    }
 
     return (
         <div className="container">
@@ -23,26 +32,26 @@ export default function SignupForm() {
                 <form className="form-wrapper">
                     <div className="name">
                         <label className="label">Parent's Full Name</label>
-                        <input className="input" type="text" value={signUpValues.parentName} />
+                        <input className="input" type="text" name="parentName" value={signUpValues.parentName} onChange={handleChange}/>
                     </div>
                     <div className="email">
                         <label className="label">Email</label>
-                        <input className="input" type="email" value={signUpValues.email} />
+                        <input className="input" type="email" name="email" value={signUpValues.email}onChange={handleChange}/>
                     </div>
                     <div className="password">
                         <label className="label">Password</label>
-                        <input className="input" type="password" value={signUpValues.password} />
+                        <input className="input" type="password" name="password" value={signUpValues.password} onChange={handleChange}/>
                     </div>
                     <div className="password">
                         <label className="label">Parent Dashboard 4 Digit Pin</label>
-                        <input className="input" type="text" value={signUpValues.parent4DigitPassword} />
+                        <input className="input" type="text" name="parent4DigitPassword" value={signUpValues.parent4DigitPassword}onChange={handleChange}/>
                     </div>
                     <div className="name">
                         <label className="label">Child's Name</label>
-                        <input className="input" type="text" value={signUpValues.childsName}/>
+                        <input className="input" type="text" name="childsName" value={signUpValues.childsName}onChange={handleChange}/>
                     </div>
                     <div>
-                        <button className="submit">Sign Up</button>
+                        <button className="submit" onClick={handleFormSubmit}>Sign Up</button>
                     </div>
                 </form>
             </div>
