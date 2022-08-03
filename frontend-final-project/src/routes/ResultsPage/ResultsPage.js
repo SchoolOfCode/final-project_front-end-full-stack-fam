b3c32a04125a27f02fc58ab959700614c1c0bc05
Wisteria Hawthorn <92452890+wisteria-hawthorn@users.noreply.github.com>
2022-08-03 11:26:56 +0100

    bug fix for NaN percentage display

--- frontend-final-project/src/routes/ResultsPage/ResultsPage.js
+++ frontend-final-project/src/routes/ResultsPage/ResultsPage.js
@@ -4,14 +4,21 @@ import { ReactComponent as MySvgNight} from '../../nighttime-jungle.svg';
 
 export default function ResultsPage({score, clicks}) {
 
+
+    let scorePercent = Math.floor((score / clicks) * 100)
+    if (isNaN(scorePercent)) {
+      scorePercent = 0;
+    };
+
     return (
       <>
        <MySvgNight className = 'results-background-image-nighttime'/>
 
         <main className = "main">
-          <h2>Well Done!</h2>
-        <h3>Your score is {score}!</h3>
-        <h3>You got {Math.floor((score / clicks) * 100)}% correct!</h3>
+          <h2 className='heading'>Well Done!</h2>
+        <h3 className='h3-score'>Your score is {score}!</h3>
+        <h3 className='h3-percentage'>You got {scorePercent}% correct</h3>
+        <h3>GREAT JOB, ADVENTURER!</h3>
         <nav>
           <Link to="/expedition">
             <button className = "map-button">Back To Map</button>
