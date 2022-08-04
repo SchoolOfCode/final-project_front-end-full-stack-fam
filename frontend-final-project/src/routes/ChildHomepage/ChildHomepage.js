97f738bdf76002428cfa9207f45367f3d027da78
NoahAldhous <naldhous@hotmail.co.uk>
2022-08-04 16:39:01 +0100

    fixed centering issue with circular progress bars, added percentage display below, added background card to bring text into focus more

--- frontend-final-project/src/routes/ChildHomepage/ChildHomepage.js
+++ frontend-final-project/src/routes/ChildHomepage/ChildHomepage.js
@@ -6,15 +6,14 @@ import LogoutButton from '../../components/Login/LogoutButton';
 import './ChildHomepage.css';
 import CircularProgressWithLabel from '@mui/material/CircularProgress';
 
-
 export default function ChildHomepage() {
 
   const navigate = useNavigate();
   const { isAuthenticated } = useAuth0();
 
-  const expOneProgress = 99;
-  const expTwoProgress = 99;
-  const expThreeProgress = 99;
+  const expOneProgress = 89;
+  const expTwoProgress = 75;
+  const expThreeProgress = 60;
   
   //If the user is not logged in, automatically redirects back to landing page.
   if(!isAuthenticated) {
@@ -26,29 +25,40 @@ export default function ChildHomepage() {
       <div className= "page-container">
         <LogoutButton/> 
         <main className= "child-homepage">
-          <section className = "child-homepage-heading-container">
-            <h1 className ='child-homepage-header'>Welcome back, <br/> Adventurer!</h1>
-            <h2 className = "child-homepage-subheader">
-              Choose your expedition!
-            </h2>
-          </section>
-          <section className="expedition-container">
-              <CircularProgressWithLabel variant="determinate" value={expOneProgress} style = {{ margin: '0', color: 'rgb(249, 189, 37)', position:'relative'}} />
-              <CircularProgressWithLabel variant="determinate" value={expTwoProgress} style = {{ margin: '0', color: 'rgb(249, 189, 37)', position:'relative'}}/>
-              <CircularProgressWithLabel variant="determinate" value={expThreeProgress} style = {{ margin: '0', color: 'rgb(249, 189, 37)', position:'relative'}} />
-          </section>
-          <section className= 'button-container'>
-            <nav>
-              <Link to="/login">
-                <button className='back-button'>Back to Login</button>
-              </Link>
-            </nav>
-            <nav>
-              <Link to="/expedition">
-                <button className='exp-button'>Choose Expedition</button>
-              </Link>
-            </nav>
-          </section>
+          <div className= "background-card">
+            <section className = "child-homepage-heading-container">
+              <h1 className ='child-homepage-header'>Welcome back, <br/> Adventurer!</h1>
+              <h2 className = "child-homepage-subheader">
+                Choose your expedition!
+              </h2>
+            </section>
+            <section className="expedition-container">
+                <section className= "expedition-card">
+                  <CircularProgressWithLabel size= '10vw' variant="determinate" value={expOneProgress} style = {{ margin: '0', color: 'rgb(249, 189, 37)', position:'relative'}} />
+                  <div className= "percentage-display">{expOneProgress}%</div>
+                </section>
+                <section className= "expedition-card">
+                  <CircularProgressWithLabel size= '10vw' variant="determinate" value={expTwoProgress} style = {{ margin: '0', color: 'rgb(249, 189, 37)', position:'relative'}} />
+                  <div className= "percentage-display">{expTwoProgress}%</div>
+                </section>
+                <section className= "expedition-card">
+                  <CircularProgressWithLabel size= '10vw' variant="determinate" value={expThreeProgress} style = {{ margin: '0', color: 'rgb(249, 189, 37)', position:'relative'}} />
+                  <div className= "percentage-display">{expThreeProgress}%</div>
+                </section>
+            </section>
+            <section className= 'button-container'>
+              <nav>
+                <Link to="/login">
+                  <button className='back-button'>Back to Login</button>
+                </Link>
+              </nav>
+              <nav>
+                <Link to="/expedition">
+                  <button className='exp-button'>Choose Expedition</button>
+                </Link>
+              </nav>
+            </section>
+          </div>
         </main>
 
       </div>
