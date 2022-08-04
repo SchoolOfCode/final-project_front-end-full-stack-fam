90e17e414b7a9fd6c426724ac93a0b399c68dc87
NoahAldhous <naldhous@hotmail.co.uk>
2022-08-04 10:59:35 +0100

    landing page 2 styled

--- frontend-final-project/src/routes/LoginPage/LoginPage.js
+++ frontend-final-project/src/routes/LoginPage/LoginPage.js
@@ -9,6 +9,9 @@ export default function LoginPage() {
   const [password, setPassword] = useState("1234");
   const navigate = useNavigate();
 
+  const childName = "Charlie"
+  const parentName = "Susan"
+
 function parentPassword() {
   let access = prompt("Please enter your password");
   if (access === password) {
@@ -21,14 +24,18 @@ function parentPassword() {
     <>
       <div id="container">
       <LogoutButton />
-
+        <section className = 'child-text-button-container'>
+        <h2 className = {"child-welcome " + "animate__animated " + "animate__pulse " + "animate__infinite " + "animate__slow " + "animate__delay-2s"}> Welcome, {childName}! Ready for adventures? </h2>
         <Link to="/child">
           <button className="child">Child Homepage</button>
         </Link>
-
-      <button className="parent" onClick={parentPassword}>
-       Parent Dashboard
-      </button>
+        </section>
+        <section className = 'parent-text-button-container'>
+        <h2 className = {"parent-welcome " + "animate__animated " + "animate__pulse " + "animate__infinite " + "animate__slow " + "animate__delay-2s"} > Welcome, {parentName}! <br/> Log in to see {childName}'s achievements.</h2>
+        <button className="parent" onClick={parentPassword}>
+        Parent Dashboard
+        </button>
+      </section>
       </div>
     </>
   );
