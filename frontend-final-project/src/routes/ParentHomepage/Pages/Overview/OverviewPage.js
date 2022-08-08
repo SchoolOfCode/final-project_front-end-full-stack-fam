d93f36fd70f7bbf9ef9d47cf243a5abb9143b088
Zaid Qarout <za.qa@outlook.com>
2022-08-08 23:31:31 +0100

    Help with css grid

--- frontend-final-project/src/routes/ParentHomepage/Pages/Overview/OverviewPage.js
+++ frontend-final-project/src/routes/ParentHomepage/Pages/Overview/OverviewPage.js
@@ -1,43 +1,46 @@
 import React from "react";
 import "./OverviewPage.css";
-import { IoPersonCircleOutline  } from "react-icons/io5";
-import { TbMathSymbols} from "react-icons/tb";
+import { IoPersonCircleOutline } from "react-icons/io5";
+import { TbMathSymbols } from "react-icons/tb";
 import BasicTabs from "./Tabs/Tabs";
 
-
-const Support= () => {
-    return (
+const Support = () => {
+  return (
     <div className="body-overview-page">
-    {/* font size on h1 would not change so I made the heading a h2 instead */}
-    <h2 className="overview-heading"> Overview </h2>
+      {/* font size on h1 would not change so I made the heading a h2 instead */}
+      <h2 className="overview-heading"> Overview </h2>
 
-    <div class="grid-container-overview">
-        
-    <div class="grid-student-profile">
-        <h2 className="subheadings-overview "> <IoPersonCircleOutline /> Student Profile </h2>
-            <h4> Name: </h4>
-            <h4> Activities Completed: </h4>
-            <h4> Progress to yearly target: </h4>
-    </div>
+      <div class="grid-container-overview">
+        <div class="grid-student-profile">
+          <h2 className="subheadings-overview ">
+            {" "}
+            <IoPersonCircleOutline /> Student Profile{" "}
+          </h2>
+          <h4> Name: </h4>
+          <h4> Activities Completed: </h4>
+          <h4> Progress to yearly target: </h4>
+        </div>
 
-    <div class="grid-topics">
-        <h2 className="subheadings-overview"> <TbMathSymbols/> Topics </h2>
-        <p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
-            Sequi dolorem deserunt minus error earum, labore perspiciatis
-             repellendus porro laborum aspernatur reprehenderit quia ducimus 
-             officia distinctio incidunt maiores explicabo suscipit optio! </p>
-    </div>
+        <div class="grid-topics">
+          <h2 className="subheadings-overview">
+            {" "}
+            <TbMathSymbols /> Topics{" "}
+          </h2>
+          <p>
+            {" "}
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sequi
+            dolorem deserunt minus error earum, labore perspiciatis repellendus
+            porro laborum aspernatur reprehenderit quia ducimus officia
+            distinctio incidunt maiores explicabo suscipit optio!{" "}
+          </p>
+        </div>
 
-    <div >
-    <BasicTabs />
+        <div class="basic-tabs">
+          <BasicTabs />
+        </div>
+      </div>
     </div>
-    
-    </div>
-
-       
-
-    </div>
-    )
+  );
 };
 
 export default Support;
