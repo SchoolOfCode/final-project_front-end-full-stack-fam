37b1542b1be9c5c648b40256e10917e2089e5d61
Wisteria Hawthorn <92452890+wisteria-hawthorn@users.noreply.github.com>
2022-08-11 22:51:53 +0100

    bananas added to container, will need to show and hide them by adding state. will complete this as a stretch goal.

--- frontend-final-project/src/routes/ActivityPage/ActivityPage.js
+++ frontend-final-project/src/routes/ActivityPage/ActivityPage.js
@@ -1,4 +1,5 @@
 // import { Link } from 'react-router-dom';
+import { React , useEffect, useState } from 'react';
 import LogoutButton from '../../components/Login/LogoutButton';
 
 import Timer from '../ActivityPage/NumLine/CountdownTimer.js';
@@ -10,7 +11,19 @@ import { ReactComponent as MySvgDay} from '../../daytime-jungle.svg';
 import { ReactComponent as MySvgNight} from '../../nighttime-jungle.svg';
 
 export default function ActivityPage({score, setScore, clicks , setClicks , mismatch , setMismatch}) {
-    
+  
+  // const [isVisible , setIsVisible] = useState(false);
+
+
+  //   useEffect(() => {
+  //     const showBanana1 = event => {
+  //     if (score <= 1) {
+  //       setIsVisible(true)
+  //     };
+
+  //     }
+
+
 
   return (
       <>
@@ -33,15 +46,18 @@ export default function ActivityPage({score, setScore, clicks , setClicks , mism
           </div>
 
           <div className="banana-container">
-            <img src="banana-line-drawing.png" alt="banana" width="50" height="50"></img>
-            <p className = "score-counter">SCORE: {score} WRONG: {mismatch}</p> 
+            <img className="banana-1" src="banana-line-drawing.png" alt="banana" width="50" height="50"></img>
+            <img className="banana-2" src="banana-line-drawing.png" alt="banana" width="50" height="50"></img>
+            <img className="banana-3" src="banana-line-drawing.png" alt="banana" width="50" height="50"></img>
           </div>
- 
         </main>
         
           <div className="the-invisible-timer">
             <Timer className='the-countdown-text' initialMinute = {0} initialSeconds = {60}/>
           </div>
+          <div className='the-score-container'>
+              <p className = "score-counter">SCORE: {score} WRONG: {mismatch}</p> 
+            </div>
 
         {/* <nav>
           <Link to="/login">Back to Log In</Link>
