6da1af22f88429916561925c36b36b47d231c4a9
Osztian42 <103217445+Osztian42@users.noreply.github.com>
2022-08-15 22:13:23 +0100

    finalcss

--- frontend-final-project/src/routes/ExpeditionPage/ExpeditionPage.js
+++ frontend-final-project/src/routes/ExpeditionPage/ExpeditionPage.js
@@ -2,12 +2,12 @@ import { Link } from 'react-router-dom'
 import LogoutButton from '../../components/Login/LogoutButton';
 import './ExpeditionPage.css'
 import './homeHut.css'
-import button from './images/homeHut.png'
+import homeHut from './images/homeHut.png'
 import woodStop from './images/woodStop.PNG'
 import './woodStop.css'
 import leftFoot from './images/leftFootCol.png'
 import rightFoot from './images/rightFootCol.png'
-import path from './images/fullPath.png'
+
 import vector from './images/idk.png'
 
 
@@ -18,24 +18,16 @@ export default function ExpeditionPage() {
       
      
       
-      <div className = 'Expedition'>
+ <div className = 'Expedition'>
+ <img src={vector} className= 'ExpeditionPage'/>
+   <h1 className = 'expeditionH1'> Addition</h1>
+   <h2 className = 'expeditionH2'>Are you ready for your next adventure?</h2>
+  
 
-
-  <img src={vector} className= 'ExpeditionPage'/>
-<h1 className = 'expeditionH1'> Addition</h1>
-<h2 className = 'expeditionH2'>Are you ready for your next adventure?</h2>
-        
-
-
-        <nav>
-          <Link to="/child"><button className = 'ChildHomeButton'>Home</button></Link>
-        </nav>
-        
-
-   
-    
- 
-          
+<nav>
+    <Link to="/child"><button className = 'ChildHomeButton'> <img src = {homeHut} className= 'hut'></img></button></Link>
+</nav>
+                
  <div className="wrap">
 <div className="upper">
   <img src={leftFoot} />
@@ -56,7 +48,7 @@ export default function ExpeditionPage() {
   <img src={rightFoot} />
   <img src={rightFoot} />
   <img src={rightFoot} />
-  <img src={rightFoot} />
+ 
   
 </div>
   </div> 
@@ -65,56 +57,17 @@ export default function ExpeditionPage() {
  
 
   <nav>
-    <Link to="/activity-intro"><button className = 'woodStopButton1'>Home</button></Link>
+    <Link to="/activity-intro"><button className = 'woodStopButton1'> <img src = {woodStop} className = 'woodButton'></img>Home</button></Link>
   </nav>
+  <img src = {woodStop} className = 'woodButton2'></img>
+  <img src = {woodStop} className = 'woodButton3'></img>
+  <img src = {woodStop} className = 'woodButton4'></img>
+  <img src = {woodStop} className = 'woodButton5'></img>
+
+
  </div>
-  {/* <div className='thatsaWrap'>
-        
-<img src={newPath} className = 'newPath'/>      
-
- 
 </div>
- 
-
-
-  {/* <div className ='stop2'>
-  {/* <img src = {path} className = 'fullPath2'/> */}
-  {/* <img src={woodStop} className = 'woodStop2'/>
-  <button className = 'woodStopButton2'></button>
-  </div> */} 
-
-
-  {/* <div className ='stop3'> */}
-  {/* <img src = {path} className = 'fullPath3'/> */}
-  {/* <img src={woodStop} className = 'woodStop3'/>
-  <button className = 'woodStopButton3'></button>
-  </div> */}
-
-
-  {/* <div className ='stop4'> */}
-  {/* <img src = {path} className = 'fullPath4'/> */}
-  {/* <img src={woodStop} className = 'woodStop4'/>
-  <button className = 'woodStopButton4'></button>
-  </div> */}
-
-
-  {/* <div className ='stop5'>
-  {/* <img src = {path} className = 'fullPath5'/> */}
-  {/* <img src={woodStop} className = 'woodStop5'/>
-  <button className = 'woodStopButton5'></button>
-  </div> */} 
-
-  {/* </div> */} 
-  
-  
-  
- 
-
-</div>
-      
-        
-
-       
+           
     
     );
   }
